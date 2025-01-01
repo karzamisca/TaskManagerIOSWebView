772d6331d34ad1bc32489b6d8d571668ed12cb59
karzamisca <miscakarza@gmail.com>
2025-01-02 00:29:03 +0700

    Initial Commit

---
+++ TaskManagerIOSWebView/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TaskManagerIOSWebView
+//
+//  Created by Minh Quan on 02/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TaskManagerIOSWebView/TaskManagerIOSWebViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerIOSWebViewApp.swift
+//  TaskManagerIOSWebView
+//
+//  Created by Minh Quan on 02/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerIOSWebViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
